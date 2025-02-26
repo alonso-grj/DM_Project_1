972465465d11aae59d3964813ea278dff01d364e
Joe El Hajj <jelhajj@smu.edu>
2025-02-25 18:34:26 -0600

    datasets and question 1

--- project1.R
+++ project1.R
@@ -1,5 +1,15 @@
-#JOe Alla 
-#test
-x<-c(2,3,4,4)
-xjnsdfkbjdsfbjkdsbj
-bhdfjhdfsjh
+library(ggplot2)
+library(dplyr)
+texas <- read.csv("~/Desktop/Data Mining/DM_Project_1/COVID-19/COVID-19_cases_TX.csv")
+mobility <- read.csv("~/Desktop/Data Mining/DM_Project_1/COVID-19/Global_Mobility_Report.csv")
+census <- COVID.19_cases_plus_census <- read.csv("~/Desktop/Data Mining/DM_Project_1/COVID-19/COVID-19_cases_plus_census.csv")
+
+#1. How did COVID-19 case numbers evolve over time in Texas?
+
+texas %>%
+  group_by(date) %>%
+  summarize(Total_Cases = sum(confirmed_cases)) %>%
+  ggplot(aes(x = date, y = Total_Cases)) +
+  geom_line(color = "blue") +
+  labs(title = "COVID-19 Case Trends in Texas", x = "Date", y = "Total Cases")
+
